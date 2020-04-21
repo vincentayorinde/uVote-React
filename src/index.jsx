b58a0f8f99ba1c91001395627b6a984150a83123
vincentayorinde <vincentayorinde@yahoo.com>
2020-04-21 18:36:27 +0100

    setup bootstrap popper js jquery

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import './styles/app.scss';
+import './styles/materia-bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'jquery/dist/jquery'
+import 'popper.js/dist/popper';
+
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/layout/Header';
+
 class App extends Component {
     render() {
-        return <h1>Welcome to uVote</h1>;
+        return <Header />;
     }
 }
 
