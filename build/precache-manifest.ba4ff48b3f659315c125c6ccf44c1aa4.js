7ba42b2ce358409068667fb2ec222e6e3bdcdc88
vincentayorinde <vincentayorinde@yahoo.com>
2020-05-09 01:19:20 +0100

    setup profile

---
+++ build/precache-manifest.ba4ff48b3f659315c125c6ccf44c1aa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5a13faada8ae705f1908cec496403c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7f5c2f639676ea9ecf",
+    "url": "/static/js/2.cae7e75f.chunk.js"
+  },
+  {
+    "revision": "c51e4fab80188393faec9ddaec528b20",
+    "url": "/static/js/2.cae7e75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7388790ea561c89fd10d",
+    "url": "/static/js/main.f9694e03.chunk.js"
+  },
+  {
+    "revision": "6df7ce26fad4019f8482",
+    "url": "/static/js/runtime-main.d3ee0efe.js"
+  }
+]);
